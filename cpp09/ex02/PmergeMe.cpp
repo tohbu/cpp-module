b82bd1a743298e7855fe14d87ed2e9e91bce76cd
tohbu <dabuyouhui@gmail.com>
2026-05-11 11:34:47 +0900

    complete PmergeMe

--- cpp09/ex02/PmergeMe.cpp
+++ cpp09/ex02/PmergeMe.cpp
@@ -1,11 +1,13 @@
-#include "PmergeMe.hpp"
-#include <algorithm>
+#include <ctime>
+#include <cstdlib>
 #include <iostream>
+#include <algorithm>
+#include <iterator>
 #include <list>
 #include <vector>
-#include <ctime>
-
-int comnum = 0;
+#include <sys/time.h>
+#include <iomanip>
+#include "PmergeMe.hpp"
 
 template < typename Iterator, typename Distance >
 Iterator my_next(Iterator it, Distance n)
@@ -22,19 +24,19 @@ Iterator my_prev(Iterator it, Distance n)
 }
 
 template < typename T >
-void printContainer(const T &container, int block_size)
+void PmergeMe< T >::printContainer(int block_size)
 {
-	for (typename T::const_iterator it = container.begin(); it != container.end(); ++it)
+	for (typename T::iterator it = _container.begin(); it != _container.end(); ++it)
 	{
 		std::cout << *it << " ";
-		if (std::distance(container.begin(), it) % block_size == block_size - 1)
+		if (std::distance(_container.begin(), it) % block_size == block_size - 1)
 			std::cout << std::endl;
 	}
 	std::cout << std::endl;
 }
 
 template < typename T >
-int PmergeMe ::findInsertionIndex(T &container, int target, typename T::iterator right_it, int block_size)
+int PmergeMe< T >::findInsertionIndex(T &container, int target, typename T::iterator right_it, int block_size)
 {
 	int low = 0;
 	int high = ((std::distance(container.begin(), right_it)) + 1);
@@ -45,7 +47,6 @@ int PmergeMe ::findInsertionIndex(T &container, int target, typename T::iterator
 		int mid_block = low + (high - low) / 2;
 		typename T::iterator mid_it = my_next(container.begin(), (mid_block * block_size) + (block_size - 1));
 		this->_nbr_of_comps++;
-		// std::cout << "Comparing target: " << target << " with mid_it: " << *mid_it << std::endl;
 		if (*mid_it < target)
 		{
 			low = mid_block + 1;
@@ -59,15 +60,15 @@ int PmergeMe ::findInsertionIndex(T &container, int target, typename T::iterator
 	}
 	return insert_block_idx;
 }
+
 template < typename T >
-void PmergeMe ::insertalgorithm(T &main_chain, T &pending_elements, int block_size)
+void PmergeMe< T >::insertalgorithm(T &main_chain, T &pending_elements, int block_size)
 {
 	size_t prev_j = 1;
 	size_t curr_j = 3;
-	size_t total_b = (pending_elements.size() / (block_size + 1)) + 1;	// bnの総数
+	size_t total_b = (pending_elements.size() / (block_size + 1)) + 1;
 	while (prev_j < total_b)
 	{
-		// 1. 今回のグループでどこまで（どのb_nまで）対象にするか決める
 		size_t actual_upper_bound = std::min(curr_j, total_b);
 		for (size_t i = actual_upper_bound; i > prev_j; --i)
 		{
@@ -76,13 +77,10 @@ void PmergeMe ::insertalgorithm(T &main_chain, T &pending_elements, int block_si
 			typename T::iterator an = my_next(pending_elements.begin(), an_pos);
 			typename T::iterator bn = my_next(pending_elements.begin(), bn_pos);
 			typename T::iterator pos;
-			// std::cout << "an = " << *an << " | " << "bn = " << *bn << std::endl;
 			if (*an != -1)
 			{
 				pos = std::find(main_chain.begin(), main_chain.end(), *an);
-				// std::cout << "Found mainchain an pos: " << *pos << std::endl;
 				pos = my_prev(pos, block_size);
-				// std::cout << "Found mainchain an-1 or bn  pos: " << *pos << std::endl;
 			}
 			else
 				pos = my_prev(main_chain.end(), 1);
@@ -90,132 +88,101 @@ void PmergeMe ::insertalgorithm(T &main_chain, T &pending_elements, int block_si
 			typename T::iterator pending_elements_pos = my_next(pending_elements.begin(), an_pos + 1);
 			typename T::iterator insert_pos = my_next(main_chain.begin(), insert_block_idx * block_size);
 			main_chain.insert(insert_pos, pending_elements_pos, my_next(pending_elements_pos, block_size));
-			// printContainer(main_chain, block_size);
 		}
-		// 3. 次のヤコブ準備
 		size_t next_j = curr_j + 2 * prev_j;
 		prev_j = curr_j;
 		curr_j = next_j;
 	}
-	// std::cout << "After inserting main elements: " << std::endl;
-	// printContainer(main_chain, block_size);
 }
 
 template < typename T >
-void PmergeMe ::make_main_chain(T &container, int block_size)
+void PmergeMe< T >::make_main_chain(int block_size)
 {
 	T main_chain;
 	T pending_elements;
 	T no_pair_elements;
-	typename T::iterator main_chain_end = my_next(container.begin(), block_size * 2);
-	main_chain.insert(main_chain.end(), container.begin(), main_chain_end);
+	typename T::iterator main_chain_end = my_next(_container.begin(), block_size * 2);
+	main_chain.insert(main_chain.end(), _container.begin(), main_chain_end);
 	size_t i = block_size * 2;
-	// main_chain と pending_elements 作成
-	for (size_t j = 0; i + block_size <= container.size(); j++, i += block_size)
+	for (size_t j = 0; i + block_size <= _container.size(); j++, i += block_size)
 	{
-		if (j % 2 == 0)	 // bn
+		if (j % 2 == 0)
 		{
-			if (i + block_size * 2 >= container.size())
+			if (i + block_size * 2 >= _container.size())
 				pending_elements.push_back(-1);
 			else
 			{
-				typename T::iterator it = my_next(container.begin(), (i + block_size * 2 - 1));
-				pending_elements.push_back(*it);  // anを先頭に挿入
+				typename T::iterator it = my_next(_container.begin(), (i + block_size * 2 - 1));
+				pending_elements.push_back(*it);
 			}
-			pending_elements.insert(pending_elements.end(), my_next(container.begin(), i), my_next(container.begin(), i + block_size));
+			pending_elements.insert(pending_elements.end(), my_next(_container.begin(), i), my_next(_container.begin(), i + block_size));
 		}
-		else  // an
-			main_chain.insert(main_chain.end(), my_next(container.begin(), i), my_next(container.begin(), i + block_size));
+		else
+			main_chain.insert(main_chain.end(), my_next(_container.begin(), i), my_next(_container.begin(), i + block_size));
 	}
-	// std::cout << "Main Chain" << std::endl;
-	// printContainer(main_chain, block_size);
-	// std::cout << "Pending Elements:" << std::endl;
-	// printContainer(pending_elements, block_size + 1);
 	if (pending_elements.empty())
 		return;
-	no_pair_elements.insert(no_pair_elements.end(), my_next(container.begin(), i), container.end());
-	/// ヤコブの挿入アルゴリズム
+	no_pair_elements.insert(no_pair_elements.end(), my_next(_container.begin(), i), _container.end());
 	insertalgorithm(main_chain, pending_elements, block_size);
-	container.clear();
-	container.insert(container.end(), main_chain.begin(), main_chain.end());
-	container.insert(container.end(), no_pair_elements.begin(), no_pair_elements.end());
+	_container.clear();
+	_container.insert(_container.end(), main_chain.begin(), main_chain.end());
+	_container.insert(_container.end(), no_pair_elements.begin(), no_pair_elements.end());
 }
 
 template < typename T >
-void PmergeMe ::sortContainer(T &container, int block_size)
+void PmergeMe< T >::sortContainer(int block_size)
 {
-	if (block_size == 1)
-		this->_nbr_of_comps = 0;
-	if (2 * block_size > (int)container.size())
+	if (2 * block_size > (int)_container.size())
 		return;
-	for (size_t i = block_size - 1; i + block_size < container.size(); i += 2 * block_size)
+	for (size_t i = block_size - 1; i + block_size < _container.size(); i += 2 * block_size)
 	{
-		typename T::iterator it_i = my_next(container.begin(), (i));
+		typename T::iterator it_i = my_next(_container.begin(), i);
 		typename T::iterator it_ipb = my_next(it_i, block_size);
 		this->_nbr_of_comps++;
 		if (*it_i > *it_ipb)
 		{
-			// block1: [i - block_size + 1] から [i] まで
-			// block2: [i + 1] から [i + block_size] まで
-			typename T::iterator block1_start = my_next(container.begin(), (i - block_size + 1));
-			typename T::iterator block1_end = my_next(container.begin(), (i + 1));
-			typename T::iterator block2_start = my_next(container.begin(), (i + 1));
+			typename T::iterator block1_start = my_next(_container.begin(), (i - block_size + 1));
+			typename T::iterator block1_end = my_next(_container.begin(), (i + 1));
+			typename T::iterator block2_start = my_next(_container.begin(), (i + 1));
 			std::swap_ranges(block1_start, block1_end, block2_start);
 		}
-		// std::cout << "Comparing...." << std::endl;
 	}
-	sortContainer(container, block_size * 2);
-	if (block_size * 2 < (int)container.size())
-		make_main_chain(container, block_size);
-	// std::cout << "Total comparisons: " << comnum << std::endl;
+	sortContainer(block_size * 2);
+	if (block_size * 2 < (int)_container.size())
+		make_main_chain(block_size);
 }
 
-void printResults(std::clock_t start, std::clock_t end, std::string container_type, int elements)
+template < typename T >
+
+void PmergeMe< T >::printResults()
 {
-	double duration = static_cast< double >(end - start) / CLOCKS_PER_SEC;
-	std::cout << "Time to process a range of " << elements << " elements with std:: " << container_type << " : " << duration * 1e6 << " us" << std::endl;
+	double start_us = static_cast< double >(this->_start_time.tv_sec) * 1000000.0 + static_cast< double >(this->_start_time.tv_usec);
+	double end_us = static_cast< double >(this->_end_time.tv_sec) * 1000000.0 + static_cast< double >(this->_end_time.tv_usec);
+	double duration = end_us - start_us;
+	std::cout << "Time to process a range of " << _container.size()
+			  << " elements with std::" << _container_type << ": "
+			  << std::fixed << std::setprecision(5) << duration << " us" << std::endl;
 }
 
-void printAfterbar(const std::vector< int > &vect)
-{
-	std::cout << "After: ";
-	for (size_t i = 0; i < vect.size(); ++i)
-	{
-		std::cout << vect[i] << " ";
-	}
-	std::cout << " (" << vect.size() << " elements)" << std::endl;
-}
-PmergeMe ::PmergeMe() : _nbr_of_comps(0)
+template < typename T >
+PmergeMe< T >::PmergeMe() : _nbr_of_comps(0)
 {
 }
 
-PmergeMe ::~PmergeMe()
+template < typename T >
+PmergeMe< T >::~PmergeMe()
 {
 }
 
-PmergeMe ::PmergeMe(int argc, char *argv[]) : _nbr_of_comps(0)
+template < typename T >
+PmergeMe< T >::PmergeMe(int argc, char *argv[], T container, std::string container_type) : _nbr_of_comps(0), _container(container), _container_type(container_type)
 {
-	std::vector< int > vect;
-	std::list< int > lst;
 	for (int i = 1; i < argc; ++i)
 	{
 		int num = std::atoi(argv[i]);
-		vect.push_back(num);
-		lst.push_back(num);
+		this->_container.push_back(num);
 	}
-	// vector
-	std::clock_t start = std::clock();
-	sortContainer(vect, 1);
-	std::clock_t end = std::clock();
-	printAfterbar(vect);
-	printResults(start, end, "vector", vect.size());
-	std::cout << "Comparisons: " << this->_nbr_of_comps << std::endl;
-	//
-	/// list
-	start = std::clock();
-	sortContainer(lst, 1);
-	end = std::clock();
-	printResults(start, end, "list", lst.size());
-	std::cout << "Comparisons: " << this->_nbr_of_comps << std::endl;
-	//
+	gettimeofday(&this->_start_time, NULL);
+	sortContainer(1);
+	gettimeofday(&this->_end_time, NULL);
 }
--- cpp09/ex02/main.cpp
+++ cpp09/ex02/main.cpp
@@ -29,7 +29,13 @@ bool isValidateInput(int argc, char *argv[])
 	return true;
 }
 
+//test case
+// ./ PmergeMe 11 2 17 0 16 8 6 15 10 3 21 1 18 9 14 19 12 5 4 20 13 7
+
 #include "PmergeMe.hpp"
+#include <list>
+#include <vector>
+
 int main(int argc, char *argv[])
 {
 	if (argc < 2)
@@ -45,36 +51,11 @@ int main(int argc, char *argv[])
 		std::cout << argv[i] << " ";
 	}
 	std::cout << " (" << argc - 1 << " elements)" << std::endl;
-	PmergeMe pm(argc, argv);
+	PmergeMe< std::vector< int > > pm(argc, argv, std::vector< int >(), "vector");
+	PmergeMe< std::list< int > > pm2(argc, argv, std::list< int >(), "list");
+	std::cout << "After: ";
+	pm.printContainer(argc);
+	pm.printResults();
+	pm2.printResults();
 	return 0;
 }
-
-//test case
-// ./ PmergeMe 11 2 17 0 16 8 6 15 10 3 21 1 18 9 14 19 12 5 4 20 13 7
-
-// tpp提出OKだったらこっちのほうがオブジェクト指向っぽい
-// #include "TPmergeMe.hpp"
-// #include <list>
-// #include <vector>
-
-// int main(int argc, char *argv[])
-// {
-// 	if (argc < 2)
-// 	{
-// 		std::cerr << "Usage: " << argv[0] << " <numbers...>" << std::endl;
-// 		return 1;
-// 	}
-// 	if (!isValidateInput(argc, argv))
-// 		return 1;
-// 	std::cout << "Before: ";
-// 	for (int i = 1; i < argc; ++i)
-// 	{
-// 		std::cout << argv[i] << " ";
-// 	}
-// 	std::cout << " (" << argc - 1 << " elements)" << std::endl;
-// 	PmergeMe< std::vector< int > > pm(argc, argv, std::vector< int >(), "vector");
-// 	PmergeMe< std::list< int > > pm2(argc, argv, std::list< int >(), "list");
-// 	pm.printContainer(pm.getContainer(), argc - 1);
-// 	pm2.printContainer(pm2.getContainer(), argc - 1);
-// 	return 0;
-// }
